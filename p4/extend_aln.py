d99bbb9f9cfeab6bd678ba0eee67093235fd2434
Maria <mariaropat@gmail.com>
2018-05-10 21:32:04 +0200

    trees

---
+++ p4/extend_aln.py
@@ -0,0 +1,17 @@
+from Bio import AlignIO
+import os
+
+
+def extended(files):
+    for n in range(4):
+        org = ""
+        metalist = ""
+        for file in os.listdir(files):
+            alignment = AlignIO.read(files+"/" + file, "fasta")
+            org = alignment[n].id.split("_")[0]
+            metalist = metalist + str(alignment[n].seq.strip())
+        with open ("all_metagenes.fasta", "a") as wh:
+            wh.write(">"+org + "\n" + metalist + "\n")
+
+if __name__ == "__main__":          
+        extended("p4/aligned")
