9c98b4901ee868327f584458e6b74d88fd0c9898
Maria <mariaropat@gmail.com>
2018-05-21 08:32:04 +0200

    upload

--- p4/scripts/extend_aln.py
+++ p4/scripts/extend_aln.py
@@ -11,8 +11,8 @@ def meta_alignment(my_dir):
             alignment = AlignIO.read(my_dir+"/" + file, "fasta")
             org = alignment[n].id.split("_")[0]
             metalist = metalist + str(alignment[n].seq.strip())
-        with open ("10_metagenes.fasta", "a") as wh:
+        with open ("conserved_metagenes.fasta", "a") as wh:
             wh.write(">"+org + "\n" + metalist + "\n")
 
 if __name__ == "__main__":          
-        meta_alignment("p4/10seq")
+        meta_alignment("p4/inversion")
