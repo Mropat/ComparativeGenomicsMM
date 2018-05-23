3f1ca2ae7e74544e9e8183c2c6a776383920cd74
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-05-23 17:00:25 +0200

    gc content, dinucleotide, ORF (almost done)

--- Final_project/gc_calc.py
+++ Final_project/gc_calc.py
@@ -21,5 +21,35 @@ def stats(genome_file):
     print('Total nucleotides(excluding undefined) =' + ' ' + str(total))
     print('Undefined nucleotides:' + ' ' + str(undefined))        
     print('GC-content =' + ' ' + str(gccontent)) 
+    
+def reverse_complement(genome_file):
+    seq = parse(genome_file)
+    replaced = seq.replace("G", "c").replace("C", "g").replace("A", "t").replace("T", "a").upper()
+    return replaced
+
+def ORF_finder(genome_file):
+    forward_strand = parse(genome_file)
+    reverse_strand = reverse_complement(genome_file)
+    reverse_strand = reverse_strand[::-1]
+    fwd_1 = []
+    fwd_2 = []
+    fwd_3 = []
+    rev_1 = []
+    rev_2 = []
+    rev_3 = []
+    for j in range(0,len(forward_strand), 3):
+        fwd_1.append(forward_strand[j:j+3])
+        fwd_2.append(forward_strand[j+1:j+4])
+        fwd_3.append(forward_strand[j+2:j+5])
+        rev_1.append(reverse_strand[j:j+3])
+        rev_2.append(reverse_strand[j+1:j+4])
+        rev_3.append(reverse_strand[j+2:j+5])
         
-stats("/afs/pdc.kth.se/misc/pdc/volumes/sbc/prj.sbc.dmessina.5/Comparative_Genomics/data/genomes2018/Grp4/04.fa.txt")
+
+
+
+reverse_complement("/afs/pdc.kth.se/misc/pdc/volumes/sbc/prj.sbc.dmessina.5/Comparative_Genomics/data/genomes2018/Grp4/04.fa.txt")
+ORF_finder("/afs/pdc.kth.se/misc/pdc/volumes/sbc/prj.sbc.dmessina.5/Comparative_Genomics/data/genomes2018/Grp4/04.fa.txt")
+#==============================================================================
+# stats("/afs/pdc.kth.se/misc/pdc/volumes/sbc/prj.sbc.dmessina.5/Comparative_Genomics/data/genomes2018/Grp4/04.fa.txt")
+#==============================================================================
