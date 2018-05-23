060ae7a47b6a4c4af9a70f6c656eec13f40f6d07
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-05-23 10:25:28 +0200

    makes plot look nice

--- practical_8/scripts/plot_scatter.py
+++ practical_8/scripts/plot_scatter.py
@@ -15,6 +15,16 @@ def plotting_prep(filename):
             cnt[i] += 1    
         freq = {key: len(list(value)) for key, value in itertools.groupby(sorted(cnt.values()))}
         for k, v in freq.items():
-            plt.scatter(np.log(k),np.log(v))
+            plt.scatter(k,v,c='red')
+        ax = plt.axes()
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        plt.xlabel('node degree')
+        plt.ylabel('frequency')
+        plt.title(filename)
         plt.show()    
 plotting_prep('B_japonicum.txt')
+plotting_prep('C_trachomatis.txt')
+plotting_prep('D_turgidum.txt')
+plotting_prep('R_baltica.txt')
+plotting_prep('S_cerevisiae.txt')
