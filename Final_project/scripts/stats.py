7559f9b00d4d371c15f0f71458987c06ac13cf5a
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-05-30 16:02:17 +0200

    aa freq

--- Final_project/scripts/stats.py
+++ Final_project/scripts/stats.py
@@ -22,18 +22,33 @@ def DNA_stats(genome_file):
     gcontent = (seq.count("G"))
     ccontent = (seq.count("C"))
     undefined = (seq.count("N"))
-    total = (len(seq)-undefined)
-    gccontent = ((gcontent + ccontent)/total)
+    total = len(seq)
+    gccontent = ((gcontent + ccontent)/(total-undefined))
     listofDI = ["AG", "AA", "AC", "AT","CG", "CA", "CC", "CT","GG", "GA", "GC", 
-    "GT", "TG", "TA", "TC", "TT" ]       
-    print('Total nucleotides(excluding undefined) =' + ' ' + str(total))
+    "GT", "TG", "TA", "TC","TT"]       
+    print('Total nucleotides(excluding undefined) =' + ' ' + str(total-undefined))
     print('Undefined nucleotides:' + ' ' + str(undefined))        
     print('GC-content =' + ' ' + str(gccontent))
     print('Dinucleotide contents:')
     for di in listofDI:        
-        print("{} content = {}".format(di, seq.count(di)/(total-1)))    
+        #print("{} content = {}".format(di, seq.count(di)/(total-1-2* undefined)))    
+        print(seq.count(di)/(total- 2*undefined-1))
 
 def protein_stats(proteome_file):
-    
+    seq = ""    
+    for record in SeqIO.parse(proteome_file, "fasta"):
+        sequences = record.seq 
+        seq = seq+str(sequences)
+    #print(seq)
+    s = ProteinAnalysis(str(seq))
+    dic = s.get_amino_acids_percent()
+    print(dic)
+    for k, v in sorted(dic.items()):
+        print(dic[k])
 if __name__ == "__main__":    
-    DNA_stats("/afs/pdc.kth.se/misc/pdc/volumes/sbc/prj.sbc.dmessina.5/Comparative_Genomics/data/genomes2018/Grp4/34.fa.txt")
+    
+    protein_stats("04_prot.fasta")
+    protein_stats("05_prot.fasta")
+    protein_stats("08_prot.fasta")
+    protein_stats("16_prot.fasta")
+    protein_stats("34_prot.fasta")
