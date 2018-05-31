0e3f32ada3babe499a59453f25adc9b76befce7e
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-05-31 17:42:31 +0200

    tired

--- Final_project/scripts/distance_matrix.py
+++ Final_project/scripts/distance_matrix.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 29 15:11:06 2018
-
-@author: u2353
+With five genome files as input, this script creates a distance matrix
+based on GC content
 """
 import numpy as np
 import math
 
+
 def parse(genome_file):
-    
+    '''Parses genome file into string'''
     with open(genome_file, 'r') as g:
         seq = ""
         for line in g:
@@ -16,17 +16,21 @@ def parse(genome_file):
                 seq += line
     return seq
     
+
 def stats(genome_file):
+    '''Calculates the GC content, excluding undefined'''
     seq = parse(genome_file)
     gcontent = (seq.count("G"))
     ccontent = (seq.count("C"))
     undefined = (seq.count("N"))
     total = (len(seq)-undefined)
     GCcontent = ((gcontent + ccontent)/total)
+    
     return GCcontent
 
  
 def distance_matrix(g1, g2, g3, g4, g5):
+    '''Creates a distance matrix of the five genomes.'''
     m = np.zeros((5,5))
     GCfreqs = [stats(g1), stats(g2), stats(g3), stats(g4), stats(g5)]
     
@@ -45,7 +49,7 @@ def distance_matrix(g1, g2, g3, g4, g5):
     distance_list = [g1_dist] + [g2_dist] + [g3_dist] + [g4_dist] + [g5_dist]
     m = np.asarray(distance_list)
     print(m)
-    
+    return m
     
 if __name__ == "__main__":
     distance_matrix('04.fa.txt', '05.fa.txt', '08.fa.txt', '16.fa.txt', '34.fa.txt')
--- Final_project/scripts/hist.py
+++ Final_project/scripts/hist.py
@@ -5,27 +5,38 @@ Created on Thu May 31 14:38:29 2018
 @author: u2353
 """
 from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
 import matplotlib.pyplot as plt
 import numpy as np
 
-seq = ""
-seqs = []
-lengths = []        
-for record in SeqIO.parse('../data/yeast_pred.fasta', "fasta"):
-    sequences = record.seq
-    #print(record)
-    #seq = seq+str(sequences)
-    seqs.append(str(sequences))
-print(seqs[0])
-for i in seqs:
-    lengths.append(len(i))
-    
 
-x = np.asarray(lengths)
-print(x)
-plt.hist(x, bins=20)
-plt.xlabel('Protein length (#AA)')
-plt.ylabel('Frequency')
-plt.show()
-#print(seqs)
+def making_array(file):
+    seqs = []
+    lengths = []        
+    for record in SeqIO.parse(file, "fasta"):
+        sequences = record.seq
+        #print(record)
+        #seq = seq+str(sequences)
+        seqs.append(str(sequences))
+    #print(seqs[0])
+    for i in seqs:
+        lengths.append(len(i))
+        
+    
+    x = np.asarray(lengths)
+    return x
+
+def plotting(f1, f2, f3, f4, f5):
+    l = [making_array(f1),  making_array(f2), making_array(f3), making_array(f4), making_array(f5)]
+    #print(l)    
+    #x = np.stack(l)  
+    colors = ['red', 'purple', 'lime', 'blue', 'pink']
+    species = ['B. japonicum', 'C. trachomatis', 'D. turgidum', 'R. baltica', 'S. cervisiae']
+
+    plt.hist(l, bins=20, histtype='step', color=colors, label=species, linewidth=1.5)
+    plt.legend(prop={'size': 10})
+    plt.xlabel('Protein length (#AA)')
+    plt.ylabel('Frequency')
+    plt.show()
+    #print(seqs)
+    
+plotting('../data/predicted_04_ent.txt', '../data/predicted_05_ent.txt', '../data/predicted_08_ent.txt', '../data/predicted_16_ent.txt', '../data/yeast_pred.fasta')
