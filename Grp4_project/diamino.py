bda530a3cc0867e3ad1375c91c89f9ffb2f35917
Maria <mariaropat@gmail.com>
2018-06-01 10:22:43 +0200

    automatic push

---
+++ Grp4_project/diamino.py
@@ -0,0 +1,31 @@
+#Script calculating diamino acid frequencies in each genome
+
+filename = "project/Ref.proteomes/rhodo.fasta"
+
+proteome = []
+diaminos = {}
+with open(filename , "r") as fh:
+    for line in fh:
+        if line.startswith(">"):
+             continue
+        else:
+            proteome.append(line.strip())
+
+dia_counter = 0
+
+for seq in proteome:
+    dia_count = len(seq)-1
+    dia_counter += dia_count
+
+    for aac in range(dia_count):
+        diamino = seq[aac] + seq[aac+1]
+        if diamino in diaminos:
+            diaminos[diamino] += 1
+        else:
+            diaminos[diamino] = 1
+
+for key in diaminos:
+    diaminos[key] = diaminos[key]/dia_counter
+
+with open("diaminos.txt", "a") as wh:
+    wh.write(str(filename) + "\n" + str(diaminos) + "\n")
---
+++ Grp4_project/distance_matrix.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+With five genome files as input, this script creates a distance matrix
+based on GC content
+"""
+import numpy as np
+import math
+
+
+def parse(genome_file):
+    '''Parses genome file into string'''
+    with open(genome_file, 'r') as g:
+        seq = ""
+        for line in g:
+            if '>' not in line:
+                seq += line
+    return seq
+    
+
+def stats(genome_file):
+    '''Calculates the GC content, excluding undefined'''
+    seq = parse(genome_file)
+    gcontent = (seq.count("G"))
+    ccontent = (seq.count("C"))
+    undefined = (seq.count("N"))
+    total = (len(seq)-undefined)
+    GCcontent = ((gcontent + ccontent)/total)
+    
+    return GCcontent
+
+ 
+def distance_matrix(g1, g2, g3, g4, g5):
+    '''Creates a distance matrix of the five genomes.'''
+    m = np.zeros((5,5))
+    GCfreqs = [stats(g1), stats(g2), stats(g3), stats(g4), stats(g5)]
+    
+    g1_dist = []
+    g2_dist = []
+    g3_dist = []
+    g4_dist = []
+    g5_dist = []
+    for i in range(len(GCfreqs)):
+        g1_dist.append(math.sqrt((GCfreqs[0]-GCfreqs[i])**2)*100)
+        g2_dist.append(math.sqrt((GCfreqs[1]-GCfreqs[i])**2)*100)
+        g3_dist.append(math.sqrt((GCfreqs[2]-GCfreqs[i])**2)*100)
+        g4_dist.append(math.sqrt((GCfreqs[3]-GCfreqs[i])**2)*100)
+        g5_dist.append(math.sqrt((GCfreqs[4]-GCfreqs[i])**2)*100)
+
+    distance_list = [g1_dist] + [g2_dist] + [g3_dist] + [g4_dist] + [g5_dist]
+    m = np.asarray(distance_list)
+    print(m)
+    return m
+    
+if __name__ == "__main__":
+    distance_matrix('04.fa.txt', '05.fa.txt', '08.fa.txt', '16.fa.txt', '34.fa.txt')
